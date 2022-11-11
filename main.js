4fa1f80734e955fb0a455157a2e287bf33b2b359
matt <yozoraworks@gmail.com>
2022-11-11 17:13:05 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const webp = require('webp-converter');
+
+//webp.grant_permission();
+
+function replaceExtension(fileName, newExtension) {
+    var i = fileName.lastIndexOf('.');
+    return fileName.substr(0, i) + newExtension;
+}
+
+function processFolder(path) {
+    let files = fs.readdirSync(path);
+
+    files.forEach(file => {
+        if (fs.statSync(path + '\\' + file).isDirectory()) {
+            processFolder(path + '\\' + file);
+        } else {
+            if (file.endsWith(".jfif")) {
+                console.log(file);
+                fs.renameSync(path + "\\" + file, path + "\\" + replaceExtension(file, ".jpg"));
+            }
+            if (file.endsWith(".webp")) {
+                console.log(file);
+                const result = webp.dwebp(path + "\\" + file, path + "\\" + replaceExtension(file, ".png"), "-o", logging = "-v");
+                result.then((response) => {
+                    fs.unlinkSync(path + "\\" + file);
+                });
+            }
+        }
+    });
+}
+
+processFolder("Y:\\Images");
